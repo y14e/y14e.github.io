9d7ba76be23c562733935b871eacb407e5ac6711
Yusuke Kamiyamane <y14e@yusukekamiyamane.com>
2024-12-31 15:31:21 +0900

    Update text-splitter.js

--- text-splitter.js/text-splitter.js
+++ text-splitter.js/text-splitter.js
@@ -1,6 +1,4 @@
-const NOBR_PREFERRED_REGEXP = /[[^\p{sc=Han}\p{sc=Hang}\p{sc=Hira}\p{sc=Kana}]&&[\p{L}\u0021-\u002C\u002E-\u003E\u0040\u005B-\u0060\u007B-\u007E\u00A0]]+/gv;
-const NOBR_FALLBACK_REGEXP = /[\p{sc=Latn}\p{sc=Cyrl}\u0021-\u002C\u002E-\u003E\u0040\u005B-\u0060\u007B-\u007E\u00A0]+/gv;
-const NOBR_REGEXP = NOBR_PREFERRED_REGEXP.test('a') ? NOBR_PREFERRED_REGEXP : NOBR_FALLBACK_REGEXP;
+const NOBR_REGEXP = /[[\P{sc=Han}]&&[\P{sc=Hang}]&&[\P{sc=Hira}]&&[\P{sc=Kana}]&&[\p{L}\u0021-\u002C\u002E-\u003E\u0040\u005B-\u0060\u007B-\u007E\u00A0]]+/gv;
 const LBR_PROHIBIT_START_REGEXP = /^[[\p{Pd}--―]\p{Pe}\p{Pf}\p{Po}\u00A0々ぁぃぅぇぉっゃゅょゎゕゖ゛゜ゝゞァィゥェォッャュョヮヵヶーヽヾㇰㇱㇲㇳㇴㇵㇶㇷㇸㇹㇺㇻㇼㇽㇾㇿ]|\p{Pi}/v;
 const LBR_PROHIBIT_END_REGEXP = /[\p{Pf}\p{Pi}\p{Ps}\p{Sc}]$/u;
 const LBR_INSEPARATABLE_REGEXP = /[―‥…]/u;
