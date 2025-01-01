54eef5b5e5f611ba32dad3186b518e9aeb228a96
Yusuke Kamiyamane <y14e@yusukekamiyamane.com>
2025-01-02 03:32:36 +0900

    Update accordion.js

--- accordion.js/accordion.js
+++ accordion.js/accordion.js
@@ -25,8 +25,6 @@ class Accordion {
       });
   }
   toggle(trigger, open) {
-    trigger.ariaExpanded = open;
-    trigger.dataset.accordionTransitioning = '';
     const panel = trigger.closest('[data-accordion-header]').nextElementSibling;
     panel.hidden = false;
     const height = `${panel.scrollHeight}px`;
@@ -41,6 +39,8 @@ class Accordion {
       panel.style.maxHeight = panel.style.overflow = '';
       this.removeEventListener('transitionend', once);
     });
+    trigger.ariaExpanded = open;
+    trigger.dataset.accordionTransitioning = '';
     panel.style.maxHeight = open ? 0 : height;
     panel.style.overflow = 'clip';
     requestAnimationFrame(() => {
