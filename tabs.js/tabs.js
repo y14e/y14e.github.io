ee7077ed04b842cae84d606030e38aa6c868b3f3
Yusuke Kamiyamane <y14e@yusukekamiyamane.com>
2025-01-03 01:28:41 +0900

    Update tabs.js

--- tabs.js/tabs.js
+++ tabs.js/tabs.js
@@ -12,43 +12,42 @@ class Tabs {
     this.lists = this.element.querySelectorAll(`[role="tablist"]${NOT_NESTED}`);
     this.tabs = this.element.querySelectorAll(`[role="tab"]${NOT_NESTED}`);
     this.panels = this.element.querySelectorAll(`[role="tabpanel"]${NOT_NESTED}`);
-    this.lists
-      .forEach((list, index) => {
-        if (index === 0) {
-          list.querySelectorAll('[role="tab"]')
-            .forEach((tab, index) => {
-              tab.id = tab.id || `tab-${getUUID()}`;
-              this.panels[index].setAttribute('aria-labelledby', `${this.panels[index].getAttribute('aria-labelledby') || ''} ${tab.id}`.trim());
-            });
-        } else if (this.options.avoidDuplicates) {
-          list.ariaHidden = true;
-        }
-        list.addEventListener('keydown', event => {
-          this.keydown(event);
+    this.initialize();
+  }
+  initialize() {
+    this.lists.forEach((list, index) => {
+      if (index === 0) {
+        list.querySelectorAll('[role="tab"]').forEach((tab, index) => {
+          tab.id = tab.id || `tab-${getUUID()}`;
+          this.panels[index].setAttribute('aria-labelledby', `${this.panels[index].getAttribute('aria-labelledby') || ''} ${tab.id}`.trim());
         });
+      } else if (this.options.avoidDuplicates) {
+        list.ariaHidden = true;
+      }
+      list.addEventListener('keydown', event => {
+        this.keydown(event);
       });
-    this.tabs
-      .forEach(tab => {
-        tab.tabIndex = tab.ariaSelected === 'true' ? 0 : -1;
-        tab.addEventListener('click', event => {
-          this.click(event);
+    });
+    this.tabs.forEach(tab => {
+      tab.tabIndex = tab.ariaSelected === 'true' ? 0 : -1;
+      tab.addEventListener('click', event => {
+        this.click(event);
+      });
+    });
+    this.panels.forEach((panel, index) => {
+      panel.id = panel.id || `tab-panel-${getUUID()}`;
+      [...this.tabs]
+        .filter((_, i) => i % (this.tabs.length / this.lists.length) === index)
+        .forEach(tab => {
+          tab.setAttribute('aria-controls', panel.id);
         });
+      if (panel.hidden) {
+        panel.tabIndex = 0;
+      }
+      panel.addEventListener('beforematch', event => {
+        this.beforematch(event);
       });
-    this.panels
-      .forEach((panel, index) => {
-        panel.id = panel.id || `tab-panel-${getUUID()}`;
-        [...this.tabs]
-          .filter((_, i) => i % (this.tabs.length / this.lists.length) === index)
-          .forEach(tab => {
-            tab.setAttribute('aria-controls', panel.id);
-          });
-        if (panel.hidden) {
-          panel.tabIndex = 0;
-        }
-        panel.addEventListener('beforematch', event => {
-          this.beforematch(event);
-        });
-      });
+    });
   }
   keydown(event) {
     const list = event.currentTarget;
@@ -63,7 +62,16 @@ class Tabs {
     const tabs = list.querySelectorAll('[role="tab"]');
     const index = [...tabs].indexOf(document.activeElement);
     const length = tabs.length;
-    const tab = tabs[key === previous ? (index - 1 < 0 ? length - 1 : index - 1) : key === next ? (index + 1) % length : key === 'Home' ? 0 : length - 1];
+    const tab =
+      tabs[
+        key === previous ?
+          index - 1 < 0 ?
+            length - 1
+          : index - 1
+        : key === next ? (index + 1) % length
+        : key === 'Home' ? 0
+        : length - 1
+      ];
     tab.focus();
     if (this.options.autoActivation) {
       tab.click();
@@ -79,16 +87,15 @@ class Tabs {
         tab.ariaSelected = selected;
         tab.tabIndex = selected ? 0 : -1;
       });
-    [...this.panels]
-      .forEach(panel => {
-        if (panel.id === id) {
-          panel.removeAttribute('hidden');
-          panel.tabIndex = 0;
-        } else {
-          panel.hidden = 'until-found';
-          panel.removeAttribute('tabindex');
-        }
-      });
+    [...this.panels].forEach(panel => {
+      if (panel.id === id) {
+        panel.removeAttribute('hidden');
+        panel.tabIndex = 0;
+      } else {
+        panel.hidden = 'until-found';
+        panel.removeAttribute('tabindex');
+      }
+    });
   }
   beforematch(event) {
     document.querySelector(`[aria-controls="${event.currentTarget.id}"]`).click();
