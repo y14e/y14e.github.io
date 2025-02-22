88a1622fce7c7b5cf91cbe6a3fd2f942d1f53217
Yusuke Kamiyamane <y@y14e.com>
2025-02-22 20:13:38 +0900

    Update tabs.js

--- tabs.js/tabs.js
+++ tabs.js/tabs.js
@@ -181,7 +181,7 @@ class TabsIndicator {
 
   initialize() {
     new ResizeObserver(() => this.update()).observe(this.list);
-    new MutationObserver(() => this.update()).observe(this.list, { attributes: true, attributeFilter: ['aria-selected'], subtree: true });
+    new MutationObserver(() => this.update()).observe(this.list, { attributeFilter: ['aria-selected'], subtree: true });
   }
 
   update() {
