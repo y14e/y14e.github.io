104fd89ed8802e518b21d26a25570de153791315
Yusuke Kamiyamane <y@y14e.com>
2025-03-07 20:49:53 +0900

    20250307

--- menu-js/menu.js
+++ menu-js/menu.js
@@ -2,8 +2,8 @@ class Menu {
   static hasOpen = {};
 
   constructor(root, options) {
-    this.root = root;
-    if (this.root.hasAttribute('data-menu-name')) this.name = this.root.getAttribute('data-menu-name') || '';
+    this.rootElement = root;
+    if (this.rootElement.hasAttribute('data-menu-name')) this.name = this.rootElement.getAttribute('data-menu-name') || '';
     this.defaults = {
       selector: {
         button: '[data-menu-button]',
@@ -15,35 +15,36 @@ class Menu {
       selector: { ...this.defaults.selector, ...options?.selector },
     };
     const NOT_NESTED = `:not(:scope ${this.settings.selector.item} *)`;
-    this.button = this.root.querySelector(`${this.settings.selector.button}${NOT_NESTED}`);
-    this.list = this.root.querySelector(`${this.settings.selector.list}${NOT_NESTED}`);
-    this.items = this.root.querySelectorAll(`${this.settings.selector.item}${NOT_NESTED}`);
-    if (!this.list || !this.items.length) return;
+    this.buttonElement = this.rootElement.querySelector(`${this.settings.selector.button}${NOT_NESTED}`);
+    this.listElement = this.rootElement.querySelector(`${this.settings.selector.list}${NOT_NESTED}`);
+    this.itemElements = this.rootElement.querySelectorAll(`${this.settings.selector.item}${NOT_NESTED}`);
+    if (!this.listElement || !this.itemElements.length) return;
     this.itemsByInitial = {};
-    if (this.name) Menu.hasOpen[this.name] ||= false;
+    if (this.name && this.isFocusable(this.buttonElement)) Menu.hasOpen[this.name] ||= false;
     this.initialize();
   }
 
   initialize() {
     document.addEventListener('mousedown', event => {
-      if (!this.root.contains(event.target)) this.handleOutsideMouseDown();
+      if (!this.rootElement.contains(event.target)) this.handleOutsideMouseDown();
     });
-    this.root.addEventListener('focusout', event => this.handleRootFocusOut(event));
-    if (this.button) {
+    this.rootElement.addEventListener('focusout', event => this.handleRootFocusOut(event));
+    if (this.buttonElement) {
       const id = Math.random().toString(36).slice(-8);
-      this.button.setAttribute('id', this.button.getAttribute('id') || `menu-button-${id}`);
-      this.list.setAttribute('id', this.list.getAttribute('id') || `menu-list-${id}`);
-      this.button.setAttribute('aria-controls', this.list.getAttribute('id'));
-      this.button.setAttribute('aria-expanded', 'false');
-      this.button.setAttribute('aria-haspopup', 'true');
-      this.button.setAttribute('tabindex', '0');
-      this.button.addEventListener('pointerover', event => this.handleButtonPointerOver(event));
-      this.button.addEventListener('click', event => this.handleButtonClick(event));
-      this.button.addEventListener('keydown', event => this.handleButtonKeyDown(event));
-      this.list.setAttribute('aria-labelledby', this.button.getAttribute('id'));
+      this.buttonElement.setAttribute('id', this.buttonElement.getAttribute('id') || `menu-button-${id}`);
+      this.listElement.setAttribute('id', this.listElement.getAttribute('id') || `menu-list-${id}`);
+      this.buttonElement.setAttribute('aria-controls', this.listElement.getAttribute('id'));
+      this.buttonElement.setAttribute('aria-expanded', 'false');
+      this.buttonElement.setAttribute('aria-haspopup', 'true');
+      this.buttonElement.setAttribute('tabindex', '0');
+      if (!this.isFocusable(this.buttonElement)) this.buttonElement.style.setProperty('pointer-events', 'none');
+      this.buttonElement.addEventListener('pointerover', event => this.handleButtonPointerOver(event));
+      this.buttonElement.addEventListener('click', event => this.handleButtonClick(event));
+      this.buttonElement.addEventListener('keydown', event => this.handleButtonKeyDown(event));
+      this.listElement.setAttribute('aria-labelledby', this.buttonElement.getAttribute('id'));
     }
-    this.list.addEventListener('keydown', event => this.handleListKeyDown(event));
-    this.items.forEach(item => {
+    this.listElement.addEventListener('keydown', event => this.handleListKeyDown(event));
+    this.itemElements.forEach(item => {
       const initial = item.textContent.trim().charAt(0).toLowerCase();
       if (/[a-z]/.test(initial)) {
         item.setAttribute('aria-keyshortcuts', initial);
@@ -51,33 +52,33 @@ class Menu {
       }
     });
     this.resetTabIndex();
-    this.root.setAttribute('data-menu-initialized', '');
+    this.rootElement.setAttribute('data-menu-initialized', '');
   }
 
   toggle(isOpen) {
-    if (!this.button || (this.button.getAttribute('aria-expanded') === 'true') === isOpen) return;
+    if (!this.buttonElement || (this.buttonElement.getAttribute('aria-expanded') === 'true') === isOpen) return;
     if (this.name) Menu.hasOpen[this.name] = isOpen;
-    this.button.setAttribute('aria-expanded', String(isOpen));
+    this.buttonElement.setAttribute('aria-expanded', String(isOpen));
   }
 
   resetTabIndex() {
-    this.items.forEach(item => item.removeAttribute('tabindex'));
-    this.items.forEach(item => item.setAttribute('tabindex', this.isNonDisabled(item) && [...this.items].filter(this.isNonDisabled).findIndex(item => item.getAttribute('tabindex') === '0') === -1 ? '0' : '-1'));
+    this.itemElements.forEach(item => item.removeAttribute('tabindex'));
+    this.itemElements.forEach(item => item.setAttribute('tabindex', this.isFocusable(item) && [...this.itemElements].filter(this.isFocusable).findIndex(item => item.getAttribute('tabindex') === '0') === -1 ? '0' : '-1'));
   }
 
-  isNonDisabled(element) {
+  isFocusable(element) {
     return element.getAttribute('aria-disabled') !== 'true' && !element.hasAttribute('disabled');
   }
 
   handleOutsideMouseDown() {
-    if (!this.button) return;
+    if (!this.buttonElement) return;
     this.close();
   }
 
   handleRootFocusOut(event) {
-    if (this.button && this.button.getAttribute('aria-expanded') !== 'true') return;
-    if (!this.root.contains(event.relatedTarget)) {
-      if (this.button) {
+    if (this.buttonElement && this.buttonElement.getAttribute('aria-expanded') !== 'true') return;
+    if (!this.rootElement.contains(event.relatedTarget)) {
+      if (this.buttonElement) {
         this.close();
       } else {
         this.resetTabIndex();
@@ -88,16 +89,16 @@ class Menu {
   handleButtonPointerOver(event) {
     if (event.pointerType !== 'mouse') return;
     if (this.name && Menu.hasOpen[this.name]) {
-      this.button.focus();
+      this.buttonElement.focus();
       this.open();
     }
   }
 
   handleButtonClick(event) {
     event.preventDefault();
-    const isOpen = this.button.getAttribute('aria-expanded') === 'true';
+    const isOpen = this.buttonElement.getAttribute('aria-expanded') === 'true';
     this.toggle(!isOpen);
-    const focusables = [...this.items].filter(this.isNonDisabled);
+    const focusables = [...this.itemElements].filter(this.isFocusable);
     if (!focusables.length) return;
     if (!isOpen) window.requestAnimationFrame(() => window.requestAnimationFrame(() => focusables[0].focus()));
   }
@@ -108,7 +109,7 @@ class Menu {
     event.preventDefault();
     if (!['Escape'].includes(key)) {
       this.open();
-      const focusables = [...this.items].filter(this.isNonDisabled);
+      const focusables = [...this.itemElements].filter(this.isFocusable);
       if (!focusables.length) return;
       window.requestAnimationFrame(() => window.requestAnimationFrame(() => focusables[key !== 'ArrowUp' ? 0 : focusables.length - 1].focus()));
       return;
@@ -118,19 +119,19 @@ class Menu {
 
   handleListKeyDown(event) {
     const { key, shiftKey } = event;
-    if (!this.button && shiftKey && key === 'Tab') return;
+    if (!this.buttonElement && shiftKey && key === 'Tab') return;
     const isAlpha = value => /^[a-z]$/i.test(value);
-    if (!([' ', 'Enter', 'ArrowUp', 'ArrowDown', 'Home', 'End', 'Escape'].includes(key) || (shiftKey && key === 'Tab') || (isAlpha(key) && this.itemsByInitial[key.toLowerCase()]?.filter(this.isNonDisabled).length))) return;
+    if (!([' ', 'Enter', 'ArrowUp', 'ArrowDown', 'Home', 'End', 'Escape'].includes(key) || (shiftKey && key === 'Tab') || (isAlpha(key) && this.itemsByInitial[key.toLowerCase()]?.filter(this.isFocusable).length))) return;
     event.preventDefault();
     const active = document.activeElement;
     if ([' ', 'Enter'].includes(key)) {
       active.click();
       return;
     }
-    const nonDisabledItems = [...this.items].filter(this.isNonDisabled);
+    const focusableItems = [...this.itemElements].filter(this.isFocusable);
     if (['ArrowUp', 'ArrowDown', 'Home', 'End'].includes(key)) {
-      const currentIndex = nonDisabledItems.indexOf(active);
-      const length = nonDisabledItems.length;
+      const currentIndex = focusableItems.indexOf(active);
+      const length = focusableItems.length;
       let newIndex = currentIndex;
       switch (key) {
         case 'ArrowUp':
@@ -146,19 +147,19 @@ class Menu {
           newIndex = length - 1;
           break;
       }
-      if (!this.button) {
-        nonDisabledItems[currentIndex].setAttribute('tabindex', '-1');
-        nonDisabledItems[newIndex].setAttribute('tabindex', '0');
+      if (!this.buttonElement) {
+        focusableItems[currentIndex].setAttribute('tabindex', '-1');
+        focusableItems[newIndex].setAttribute('tabindex', '0');
       }
-      nonDisabledItems[newIndex].focus();
+      focusableItems[newIndex].focus();
       return;
     }
     if (['Tab', 'Escape'].includes(key)) {
       this.close();
       return;
     }
-    const nonDisabledItemsByInitial = this.itemsByInitial[key.toLowerCase()].filter(this.isNonDisabled);
-    const index = nonDisabledItemsByInitial.findIndex(item => nonDisabledItems.indexOf(item) > nonDisabledItems.indexOf(active));
+    const nonDisabledItemsByInitial = this.itemsByInitial[key.toLowerCase()].filter(this.isFocusable);
+    const index = nonDisabledItemsByInitial.findIndex(item => focusableItems.indexOf(item) > focusableItems.indexOf(active));
     nonDisabledItemsByInitial[index !== -1 ? index : 0].focus();
   }
 
@@ -168,7 +169,7 @@ class Menu {
 
   close() {
     this.toggle(false);
-    if (this.button && this.root.contains(document.activeElement)) this.button.focus();
+    if (this.buttonElement && this.rootElement.contains(document.activeElement)) this.buttonElement.focus();
   }
 }
 
