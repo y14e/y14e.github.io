01c11efda8bf09e86f94a79881eeb16588e21b66
Yusuke Kamiyamane <y@y14e.com>
2025-06-10 23:26:23 +0900

    Update text-splitter.js

--- text-splitter-js/text-splitter.js
+++ text-splitter-js/text-splitter.js
@@ -17,11 +17,15 @@ export class TextSplitter {
     };
     this.original = this.rootElement.innerHTML;
     this.fragment = new DocumentFragment();
+    this.wordElements = [];
+    this.charElements = [];
+    this.initialize();
+  }
+
+  initialize() {
     [...this.rootElement.childNodes].forEach(node => {
       this.fragment.appendChild(node.cloneNode(true));
     });
-    this.wordElements = [];
-    this.charElements = [];
     this.nobr();
     this.split('word');
     if (this.settings.lineBreakingRules && !this.settings.concatChar) {
