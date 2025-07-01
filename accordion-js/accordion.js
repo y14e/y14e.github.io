159aca7f9b36f5678e103241572689bc90e31a47
Yusuke Kamiyamane <y@y14e.com>
2025-07-01 19:49:57 +0900

    Update accordion.js

--- accordion-js/accordion.js
+++ accordion-js/accordion.js
@@ -45,7 +45,7 @@ export class Accordion {
         trigger.setAttribute('aria-expanded', 'false');
       }
       trigger.setAttribute('id', trigger.getAttribute('id') || `accordion-trigger-${id}`);
-      trigger.setAttribute('tabindex', String(this.isFocusable(trigger) ? 0 : -1));
+      trigger.setAttribute('tabindex', this.isFocusable(trigger) ? '0' : '-1');
       if (!this.isFocusable(trigger)) {
         trigger.style.setProperty('pointer-events', 'none');
       }
@@ -80,7 +80,7 @@ export class Accordion {
     if (name) {
       const current = this.rootElement.querySelector(`[aria-expanded="true"][data-accordion-name="${name}"]`);
       if (open && current && current !== trigger) {
-        this.close(current);
+        this.toggle(current, false, match);
       }
     }
     trigger.setAttribute('aria-label', trigger.getAttribute(`data-accordion-${open ? 'expanded' : 'collapsed'}-label`) ?? (trigger.getAttribute('aria-label') || ''));
@@ -160,7 +160,7 @@ export class Accordion {
   }
 
   handleContentBeforeMatch(event) {
-    const trigger = this.rootElement.querySelector(`[aria-controls="${event.currentTarget.getAttribute('id')}"]`);
+    const trigger = this.triggerElements[this.contentElements.indexOf(event.currentTarget)];
     if (trigger.getAttribute('aria-expanded') === 'true') {
       return;
     }
