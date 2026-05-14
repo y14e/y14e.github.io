3997cf116005ee64b7174dec546c1444cfef869e
Yusuke Kamiyamane <y@y14e.com>
2026-05-14 15:54:21 +0900

    Update disclosure.js

--- disclosure-ts/disclosure.js
+++ disclosure-ts/disclosure.js
@@ -121,6 +121,10 @@ export default class Disclosure {
     });
     this.#animationController?.abort();
     this.#animationController = null;
+    this.#detailsElements.forEach((details) => {
+      details.removeAttribute('data-disclosure-name');
+      details.removeAttribute('data-disclosure-open');
+    });
     this.#detailsElements.length = 0;
     this.#summaryElements.length = 0;
     this.#contentElements.length = 0;
@@ -274,12 +278,7 @@ export default class Disclosure {
       details.setAttribute('name', name);
       details.removeAttribute('data-disclosure-name');
     }
-    if (details.hasAttribute('data-disclosure-open')) {
-      binding.timer = requestAnimationFrame(() => {
-        binding.timer = undefined;
-        details.removeAttribute('data-disclosure-open');
-      });
-    } else {
+    if (!details.hasAttribute('data-disclosure-open')) {
       details.open = false;
     }
     const { style } = content;
